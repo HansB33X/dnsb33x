e560c981127eb45a5a656ea3692071496c70344d
HansB33X <97372552+HansB33X@users.noreply.github.com>
2022-02-15 19:55:13 +0700

    Update dnslook.py

--- dnslook.py
+++ dnslook.py
@@ -4,12 +4,12 @@ import pyfiglet
 
 os.system('apt update && apt upgrade')
 os.system('pip install pyfiglet')
-
+os.system('clear')
 
 banner = pyfiglet.figlet_format("DNS LOOKUP")
-print("[+]=============================================[+]")
+print("[+]================================================[+]")
 print(banner, "by B33X")
-print("[+]=============================================[+]")
+print("[+]================================================[+]\n")
 
 url = input("url (example google.com) > ")
 ip = socket.gethostbyname(url)
